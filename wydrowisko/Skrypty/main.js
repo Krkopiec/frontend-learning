0aadaade2bcdf8728dd37b83659e1565af714bea
Krzysztof Kopiec <kr.kopiec@gmail.com>
2018-10-04 22:11:03 +0200

    Add script to showing main image

---
+++ wydrowisko/Skrypty/main.js
@@ -0,0 +1,30 @@
+var SELEKTOR_DUZEGO_OBRAZU = '[data-typ-obrazu="cel"]';
+var SELEKTOR_TYTUL_OBRAZU = '[data-typ-obrazu="tytul"]';
+var SELEKTOR_MINIATURY = '[data-typ-obrazu="wyzwalacz"]';
+function zmienobraz(urlObrazu, tekstTutul) {
+    'use strict';
+    var duzyObraz = document.querySelector(SELEKTOR_DUZEGO_OBRAZU);
+    duzyObraz.setAttribute('src', urlObrazu);
+    var tytulObrazu = document.querySelector(SELEKTOR_TYTUL_OBRAZU);
+    tytulObrazu.textContent = tekstTutul;
+}
+function obrazMiniatury(miniatura){
+    'use strict';
+    return miniatura.getAttribute('data-url-obrazu');
+}
+function tytulMiniatury(miniatura){
+    'use strict';
+    return miniatura.getAttribute('data-tytul-obrazu');
+}
+function zmienObrazNaPodstMiniatury(miniatura) {
+    'use strict';
+    zmienobraz(obrazMiniatury(miniatura), tytulMiniatury(miniatura));
+    
+}
+function dodajObslugeKliknieciaMiniatury(miniatura) {
+'use strict';
+miniatura.addEventListener('click', function(zdarzenie) {
+    zdarzenie.preventDefault();
+    zmienObrazNaPodstMiniatury(miniatura);
+});
+}
