e4dd28f4433b203a81d144402854c83fedbe32b0
Krzysztof Kopiec <kr.kopiec@gmail.com>
2018-10-08 17:05:44 +0200

    add click listener

--- wydrowisko/Skrypty/main.js
+++ wydrowisko/Skrypty/main.js
@@ -1,6 +1,7 @@
 var SELEKTOR_DUZEGO_OBRAZU = '[data-typ-obrazu="cel"]';
 var SELEKTOR_TYTUL_OBRAZU = '[data-typ-obrazu="tytul"]';
 var SELEKTOR_MINIATURY = '[data-typ-obrazu="wyzwalacz"]';
+
 function zmienobraz(urlObrazu, tekstTutul) {
     'use strict';
     var duzyObraz = document.querySelector(SELEKTOR_DUZEGO_OBRAZU);
@@ -8,23 +9,40 @@ function zmienobraz(urlObrazu, tekstTutul) {
     var tytulObrazu = document.querySelector(SELEKTOR_TYTUL_OBRAZU);
     tytulObrazu.textContent = tekstTutul;
 }
-function obrazMiniatury(miniatura){
+
+function obrazMiniatury(miniatura) {
     'use strict';
     return miniatura.getAttribute('data-url-obrazu');
 }
-function tytulMiniatury(miniatura){
+
+function tytulMiniatury(miniatura) {
     'use strict';
     return miniatura.getAttribute('data-tytul-obrazu');
 }
+
 function zmienObrazNaPodstMiniatury(miniatura) {
     'use strict';
     zmienobraz(obrazMiniatury(miniatura), tytulMiniatury(miniatura));
     
 }
+
 function dodajObslugeKliknieciaMiniatury(miniatura) {
-'use strict';
-miniatura.addEventListener('click', function(zdarzenie) {
-    zdarzenie.preventDefault();
-    zmienObrazNaPodstMiniatury(miniatura);
-});
+    'use strict';
+    miniatura.addEventListener('click', function(zdarzenie) {
+        zdarzenie.preventDefault();
+        zmienObrazNaPodstMiniatury(miniatura);
+    });
 }
+function odczytajTabliceMiniatur () {
+    'use strict';
+    var miniatury = document.querySelectorAll(SELEKTOR_MINIATURY);
+    var tablicaMiniatur = [].slice.call(miniatury);
+    return tablicaMiniatur;
+}
+function inicjujZdarzenia() {
+    'use strict';
+    var miniatury = odczytajTabliceMiniatur();
+    miniatury.forEach(dodajObslugeKliknieciaMiniatury);    
+    };
+    inicjujZdarzenia();
+
